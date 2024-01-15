87ea9f930367bf4fa267340cbd1961a15793d8a0
salahmohsen <salahmmohsen@gmail.com>
2024-01-15 12:37:41 +0200

    improvement for layout and mobile section title

--- app/components/navigation/mobileSectionTitle.jsx
+++ app/components/navigation/mobileSectionTitle.jsx
@@ -2,7 +2,7 @@
 
 const MobileSectionTitle = ({ sectionTitle }) => {
   return (
-    <div className=" sticky top-5 -ml-4 flex h-10 w-screen items-center justify-between border-b bg-night/15 px-4 backdrop-blur-3xl dark:border-faded sm:-ml-8 sm:px-8 lg:hidden ">
+    <div className=" sticky top-2  flex h-10 items-center justify-between rounded-md  bg-night/15 px-1 backdrop-blur-3xl dark:border-faded sm:-ml-8 sm:px-8 lg:hidden ">
       <p className="font-bold">{sectionTitle}</p>
     </div>
   );
